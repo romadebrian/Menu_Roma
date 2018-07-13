c2d7279ba6b90938daea7fceed817bea6e22c9b6
romadebrian <roma_coll04@yahoo.com>
2018-07-13 17:51:04 +0700

    Initial commit

---
+++ app/src/main/java/it/pradita/ac/id/menuroma/MenuUtama.java
@@ -0,0 +1,26 @@
+package it.pradita.ac.id.menuroma;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class MenuUtama extends AppCompatActivity {
+    ImageButton Profile, Tools, Settings, About;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_utama);
+        Profile = (ImageButton) findViewById(R.id.imageButton);
+
+    }
+    public void Profile_Click (View V)
+    {
+        Intent intent = new Intent(MenuUtama.this, Profil.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/it/pradita/ac/id/menuroma/Profil.java
@@ -0,0 +1,13 @@
+package it.pradita.ac.id.menuroma;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Profil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profil);
+    }
+}
